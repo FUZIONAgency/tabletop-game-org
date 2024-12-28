87a363c89590f6c220b0c5c08ef542af7176d964
Jared Nielsen <jared@fuzionagency.com>
2024-12-27 21:42:46 -0600

    updated wrapper layout for qualify pages

--- src/pages/qualify/GetCertified.tsx
+++ src/pages/qualify/GetCertified.tsx
@@ -1,20 +1,25 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-
+import Navigation from "@/components/Navigation";
 const GetCertified = () => {
   return (
-    <div className="container mx-auto px-6 py-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>Get Certified</CardTitle>
-          <CardDescription>
-            Complete your certification process to become a qualified player
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          {/* Certification content will go here */}
-          <p>Certification process content coming soon...</p>
-        </CardContent>
-      </Card>
+    <div className="min-h-screen bg-background">
+      <Navigation />
+      <main className="pt-16">
+        <div className="container mx-auto px-6 py-8">
+          <Card>
+            <CardHeader>
+              <CardTitle>Get Certified</CardTitle>
+              <CardDescription>
+                Complete your certification process to become a qualified pro
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              {/* Certification content will go here */}
+              <p>Certification process content coming soon...</p>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
     </div>
   );
 };
